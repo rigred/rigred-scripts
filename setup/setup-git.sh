158c0bd1c4f5bf18afe2d050686afa8a0293233b
Rigo Reddig <rigo.reddig@gmail.com>
2025-07-11 09:51:55 +0200

    ### Why
    Fixes the git commit message template headings and commenting style.
    
    ### What
    
    Removed # as git comment
    Replaced ; as git comment
    
    ### Testing
    
    Updated the .gitmessage and .githooks/prepare-commit-msg to ensure it's automatic.
    
    ---
    
    Signed-off-by: Rigo Reddig <rigo.reddig@gmail.com>

--- setup/setup-git.sh
+++ setup/setup-git.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 git config commit.template "$(git rev-parse --show-toplevel)/.gitmessage"
+git config core.commentChar ';'
+echo "✔ Git commit template & commentChar configured."
